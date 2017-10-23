ba2aebca27bdf3e6e5197727f12648a66c029dfe
JoaoRacedo <racedo@uninorte.edu.co>
2017-10-23 08:14:02 -0500

    Add files via upload
    characterization.py 
    solodity fix

--- characterization.py
+++ characterization.py
@@ -7,7 +7,7 @@ def properties(src):
     regionprops = measure.regionprops(src, cache = False)  # cache = False doesn't calculate the properties until you call them
     
     areas = [r.area for r in regionprops]
-    solidities = [r.area for r in regionprops]
+    solidities = [r.solidity for r in regionprops]
     eccentricities = [r.eccentricity for r in regionprops]
     _,NumObjects = measure.label(src,return_num =True)
     
